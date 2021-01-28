3d316f715849eee0f76b2160df860d14a69d585b
Quentin Kermaidic <kermabox@hotmail.fr>
2021-01-28 14:45:36 +0100

    enforce production backend

--- src/services/config/index.tsx
+++ src/services/config/index.tsx
@@ -5,11 +5,13 @@ import { Config } from '../types';
 import dev from './envs/dev.json';
 import prod from './envs/prod.json';
 
-const { manifest } = Constants;
+// TODO: enforce this
+const config = prod;
 
-const config = typeof manifest.packagerOpts === 'object' && manifest.packagerOpts.dev
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  ? { api: { url: `http://${manifest.debuggerHost!.split(':').shift()!.concat(dev.api.url)}` } }
-  : prod;
+// const { manifest } = Constants;
+// const config = typeof manifest.packagerOpts === 'object' && manifest.packagerOpts.dev
+//   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+//   ? { api: { url: `http://${manifest.debuggerHost!.split(':').shift()!.concat(dev.api.url)}` } }
+//   : prod;
 
 export default config as Config;
